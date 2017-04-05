681bcf9d56cd6066d915b1968b3dd2c5fdf5e73d
WenJiang <WenJiangPR@gmail.com>
2017-04-05 11:09:05 +0800

    Init it

---
+++ Gomoku/main.c
@@ -0,0 +1,14 @@
+/*
+*    main.c
+*    Gomoku Project
+*	 Team production.
+*	 Team member: Jiang Wen, Huang Nianmei, Li Jianing.
+*    This file was created by Wen Jiang on 4/5/2017.
+*    Copyright (c) 2017 Wen Jiang, Huang Nianmei, Li Jianing. All rights reserved.
+*/
+#include"main.h"
+
+int Setup() {
+	initWindow("Gomoku", DEFAULT, DEFAULT, 1024, 768);
+	return 0;
+}

140be7d5aae58bbbbdc9a37fa57e2cf21b70a5ad
LiJianing <sos37809@yeah.net>
2017-04-13 19:48:58 +0800

    绘图函数paint; 全局变量status的声明。

--- Gomoku/main.c
+++ Gomoku/main.c
@@ -8,10 +8,14 @@
 */
 #include"main.h"
 
-int InitialTheGame();
+
+
 
 int Setup() {
-	initWindow("Gomoku", DEFAULT, DEFAULT, 1536, 802);
+	initWindow("Gomoku", DEFAULT, DEFAULT, 1460, 750);
+	initConsole();
+	registerTimerEvent(&TimerEvent);
+	registerMouseEvent(&MouseEvent);
 	InitialTheGame();
 	return 0;
 }
@@ -19,11 +23,30 @@ int Setup() {
 int InitialTheGame()
 {
 	beginPaint();
-	ACL_Image Checkboard, DashBoard;
+
 	loadImage(".//checkboard.bmp", &Checkboard);
-	putImage(&Checkboard, 0, 0);
 //	loadImage(".//DashBoard.bmp", &DashBoard);
 //	putImage(&DashBoard, DASHBOARDX, DASHBOARDY);
 	endPaint();
+	return 0;
+}
+
+int PaintTheGame()
+{
+	putImage(&Checkboard, 0, 0);
+	return 0;
+}
+
+TimerEventCallback TimerEvent(int timerID)
+{
+	return 0;
+}
+
+MouseEventCallback MouseEvent(int x, int y, int button, int event)
+{
+	beginPaint();
+	PaintTheGame();
+	endPaint();
+	printf("x=%4d, y=%4d, butoton =%d, event = %d\n", x, y, button, event);
 	return 0;
 }
--- Gomoku/main.h
+++ Gomoku/main.h
@@ -2,6 +2,25 @@
 #define MAIN_H
 
 #include "acllib.h"
+#include "stdio.h"
 #include"AI.h"
+
+typedef int status;
+//全局变量定义如下
+status Status;
+
+//常量定义如下
+const int MENU = 0;
+const int PLAYING = 1;
+const int END = 2;
+
+ACL_Image Checkboard;//载入的图片
+
+int InitialTheGame();//载入图片
+int PaintTheGame();//绘制当前游戏画面
+TimerEventCallback TimerEvent(int timerID);
+MouseEventCallback MouseEvent(int x, int y, int button, int event);
+
+
 //Ai module
 #endif // !MAIN_H

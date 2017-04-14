a74c2c2f828da52746c82fafd6f25017b9b8c3e7
WenJiang <WenJiangPR@gmail.com>
2017-04-14 08:52:09 +0800

    Frame work contribute.

--- Gomoku/Check_Algorithm.c
+++ Gomoku/Check_Algorithm.c
@@ -1,1 +1,11 @@
+/*
+*    Check_Algorithm.c
+*    Gomoku Project
+*	 Team production.
+*	 Team member: Jiang Wen, Huang Nianmei, Li Jianing.
+*    This file was created by Wen Jiang on 4/10/2017.
+*    Copyright (c) 2017 Wen Jiang, Huang Nianmei, Li Jianing. All rights reserved.
+*/
 #include"Check_Algorithm.h"
+
+//Check who is winner.
--- Gomoku/Check_Algorithm.h
+++ Gomoku/Check_Algorithm.h
@@ -1,5 +1,17 @@
+/*
+*    Chexk_Algorithm.h
+*    Gomoku Project
+*	 Team production.
+*	 Team member: Jiang Wen, Huang Nianmei, Li Jianing.
+*    This file was created by Wen Jiang on 4/10/2017.
+*    Copyright (c) 2017 Wen Jiang, Huang Nianmei, Li Jianing. All rights reserved.
+*/
 #pragma once
 #ifndef CHECK_ALGORITHM_H
 #include "main.h"
+//VAriable define & extern.
+extern int m_Victory;
+
+int 
 #endif // !CHECK_ALGORITHM_H
 
